3ded8d38f1505f53c0e5ecba428ed641143f89cf
Eternln00b <Laude_maximilien_10@hotmail.com>
2023-07-06 20:07:49 +0200

    update 06-07-23 20:07

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -6,79 +6,89 @@ PKG_NAME="u-boot"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
-PKG_DEPENDS_TARGET="toolchain Python3:host swig:host"
+PKG_DEPENDS_TARGET="toolchain swig:host"
 PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
 
-PKG_STAMP="${UBOOT_SYSTEM} ${UBOOT_TARGET}"
+PKG_IS_KERNEL_PKG="yes"
+PKG_STAMP="$UBOOT_SYSTEM $UBOOT_TARGET"
 
-[ -n "${KERNEL_TOOLCHAIN}" ] && PKG_DEPENDS_TARGET+=" gcc-arm-${KERNEL_TOOLCHAIN}:host"
-
-if [ -n "${UBOOT_FIRMWARE}" ]; then
-  PKG_DEPENDS_TARGET+=" ${UBOOT_FIRMWARE}"
-  PKG_DEPENDS_UNPACK+=" ${UBOOT_FIRMWARE}"
+if [ -n "$UBOOT_FIRMWARE" ]; then
+  PKG_DEPENDS_TARGET+=" $UBOOT_FIRMWARE"
+  PKG_DEPENDS_UNPACK+=" $UBOOT_FIRMWARE"
 fi
 
-PKG_NEED_UNPACK="${PROJECT_DIR}/${PROJECT}/bootloader"
-[ -n "${DEVICE}" ] && PKG_NEED_UNPACK+=" ${PROJECT_DIR}/${PROJECT}/devices/${DEVICE}/bootloader"
+PKG_NEED_UNPACK="$PROJECT_DIR/$PROJECT/bootloader"
+[ -n "$DEVICE" ] && PKG_NEED_UNPACK+=" $PROJECT_DIR/$PROJECT/devices/$DEVICE/bootloader"
 
-case "${PROJECT}" in
-  Amlogic)
-    PKG_VERSION="807482107a6d426dbcd6457d9ccf8b3ce6ca887b" # 2021.04-rc2 custodians/u-boot-amlogic-test
-    PKG_SHA256="a10430d2c1a1d9e83e66bed342433ddfe4f3d6f16d9fa8b4d4c034b600baffd3"
-    PKG_URL="https://github.com/chewitt/u-boot/archive/${PKG_VERSION}.tar.gz"
-    PKG_PATCH_DIRS="amlogic"
-    ;;
+case "$PROJECT" in
   Rockchip)
-    PKG_VERSION="8659d08d2b589693d121c1298484e861b7dafc4f"
-    PKG_SHA256="3f9f2bbd0c28be6d7d6eb909823fee5728da023aca0ce37aef3c8f67d1179ec1"
-    PKG_URL="https://github.com/rockchip-linux/u-boot/archive/${PKG_VERSION}.tar.gz"
-    PKG_PATCH_DIRS="rockchip"
+    case "$DEVICE" in
+      OdroidGoAdvance)
+        PKG_VERSION="a1b59905a4554055f35196e17301bf83cbe41b5f"
+        PKG_SHA256="7cd65ce1729a204283a7c83f5a55aa500c38cc41e0acea8595981514cab77be9"
+        PKG_URL="https://github.com/hardkernel/u-boot/archive/$PKG_VERSION.tar.gz"
+        PKG_PATCH_DIRS="odroidgoadvance"
+        ;;
+      *)
+        PKG_VERSION="8659d08d2b589693d121c1298484e861b7dafc4f"
+        PKG_SHA256="3f9f2bbd0c28be6d7d6eb909823fee5728da023aca0ce37aef3c8f67d1179ec1"
+        PKG_URL="https://github.com/rockchip-linux/u-boot/archive/$PKG_VERSION.tar.gz"
+        PKG_PATCH_DIRS="rockchip"
+        ;;
+    esac
     ;;
   *)
-    PKG_VERSION="2021.01"
-    PKG_SHA256="b407e1510a74e863b8b5cb42a24625344f0e0c2fc7582d8c866bd899367d0454"
-    PKG_URL="http://ftp.denx.de/pub/u-boot/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
-    PKG_PATCH_DIRS="default"
+    PKG_VERSION="2019.04"
+    PKG_SHA256="76b7772d156b3ddd7644c8a1736081e55b78828537ff714065d21dbade229bef"
+    PKG_URL="http://ftp.denx.de/pub/u-boot/u-boot-$PKG_VERSION.tar.bz2"
     ;;
 esac
 
 post_patch() {
-  if [ -n "${UBOOT_SYSTEM}" ] && find_file_path bootloader/config; then
-    PKG_CONFIG_FILE="${PKG_BUILD}/configs/$(${ROOT}/${SCRIPTS}/uboot_helper ${PROJECT} ${DEVICE} ${UBOOT_SYSTEM} config)"
-    if [ -f "${PKG_CONFIG_FILE}" ]; then
-      cat ${FOUND_PATH} >> "${PKG_CONFIG_FILE}"
+  if [ -n "$UBOOT_SYSTEM" ] && find_file_path bootloader/config; then
+    PKG_CONFIG_FILE="$PKG_BUILD/configs/$($ROOT/$SCRIPTS/uboot_helper $PROJECT $DEVICE $UBOOT_SYSTEM config)"
+    if [ -f "$PKG_CONFIG_FILE" ]; then
+      cat $FOUND_PATH >> "$PKG_CONFIG_FILE"
     fi
+  fi
+  #host gcc 10 build issue
+  if [ -f $PKG_BUILD/scripts/dtc/dtc-lexer.l ]; then
+    sed -i '/YYLTYPE yylloc/d' $PKG_BUILD/scripts/dtc/dtc-lexer.l
+  fi
+  #Rockchip u-boot fix
+  if [ -f $PKG_BUILD/scripts/dtc/dtc-lexer.lex.c_shipped ]; then
+    sed -i '/YYLTYPE yylloc/d' $PKG_BUILD/scripts/dtc/dtc-lexer.lex.c_shipped
   fi
 }
 
 make_target() {
-  if [ -z "${UBOOT_SYSTEM}" ]; then
+  if [ -z "$UBOOT_SYSTEM" ]; then
     echo "UBOOT_SYSTEM must be set to build an image"
     echo "see './scripts/uboot_helper' for more information"
   else
     [ "${BUILD_WITH_DEBUG}" = "yes" ] && PKG_DEBUG=1 || PKG_DEBUG=0
-    DEBUG=${PKG_DEBUG} CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" ARCH=arm make mrproper
-    [ -n "${UBOOT_FIRMWARE}" ] && find_file_path bootloader/firmware && . ${FOUND_PATH}
-    DEBUG=${PKG_DEBUG} CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" ARCH=arm make HOSTCC="${HOST_CC}" HOSTCFLAGS="-I${TOOLCHAIN}/include" HOSTLDFLAGS="${HOST_LDFLAGS}" $(${ROOT}/${SCRIPTS}/uboot_helper ${PROJECT} ${DEVICE} ${UBOOT_SYSTEM} config)
-    DEBUG=${PKG_DEBUG} CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="" ARCH=arm _python_sysroot="${TOOLCHAIN}" _python_prefix=/ _python_exec_prefix=/ make ${UBOOT_TARGET} HOSTCC="${HOST_CC}" HOSTLDFLAGS="-L${TOOLCHAIN}/lib" HOSTSTRIP="true" CONFIG_MKIMAGE_DTC_PATH="scripts/dtc/dtc"
+    [ -n "$UBOOT_FIRMWARE" ] && find_file_path bootloader/firmware && . ${FOUND_PATH}
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE="$TARGET_KERNEL_PREFIX" LDFLAGS="" ARCH=arm make mrproper
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE="$TARGET_KERNEL_PREFIX" LDFLAGS="" ARCH=arm make $($ROOT/$SCRIPTS/uboot_helper $PROJECT $DEVICE $UBOOT_SYSTEM config)
+    DEBUG=${PKG_DEBUG} CROSS_COMPILE="$TARGET_KERNEL_PREFIX" LDFLAGS="" ARCH=arm _python_sysroot="$TOOLCHAIN" _python_prefix=/ _python_exec_prefix=/ make $UBOOT_TARGET HOSTCC="$HOST_CC" HOSTLDFLAGS="-L$TOOLCHAIN/lib" HOSTSTRIP="true" CONFIG_MKIMAGE_DTC_PATH="scripts/dtc/dtc"
   fi
 }
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/share/bootloader
+  mkdir -p $INSTALL/usr/share/bootloader
 
     # Only install u-boot.img et al when building a board specific image
-    if [ -n "${UBOOT_SYSTEM}" ]; then
+    if [ -n "$UBOOT_SYSTEM" ]; then
       find_file_path bootloader/install && . ${FOUND_PATH}
     fi
 
     # Always install the update script
-    find_file_path bootloader/update.sh && cp -av ${FOUND_PATH} ${INSTALL}/usr/share/bootloader
+    find_file_path bootloader/update.sh && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
 
     # Always install the canupdate script
     if find_file_path bootloader/canupdate.sh; then
-      cp -av ${FOUND_PATH} ${INSTALL}/usr/share/bootloader
-      sed -e "s/@PROJECT@/${DEVICE:-${PROJECT}}/g" \
-          -i ${INSTALL}/usr/share/bootloader/canupdate.sh
+      cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
+      sed -e "s/@PROJECT@/${DEVICE:-$PROJECT}/g" \
+          -i $INSTALL/usr/share/bootloader/canupdate.sh
     fi
 }
